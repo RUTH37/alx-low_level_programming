a9349edfcd6fc5711e1678c22bc4c931b4bfab20
RUTH37 <makoloruth19@gmail.com>
2022-03-21 21:56:14 +0100

    copies of string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string to by src, including \0.
+ * @src: pointer.
+ * @dest: pointer.
+ * Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, lenght;
+
+	for (lenght = 0; src[lenght] != '\0'; length++)
+	{
+	}
+
+	for (i = 0; i <= length ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
